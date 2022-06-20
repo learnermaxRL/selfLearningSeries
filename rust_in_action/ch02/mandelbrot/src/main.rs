bda308438c14313920f84ed353f138c2602badae
learnermaxrl <sharma.mayank2125@gmail.com>
2022-06-21 02:46:08 +0530

    adding part CPU code

--- rust_in_action/ch02/mandelbrot/src/main.rs
+++ rust_in_action/ch02/mandelbrot/src/main.rs
@@ -1,60 +1,60 @@
 use num::complex::Complex; 
 
-fn check_in_set(p:(f64,f64)) -> usize{
+// fn check_in_set(p:(f64,f64)) -> usize{
 
-    // println!("{}   , {}",p.0,p.1);
-    let mut z = Complex { re: 0.0, im: 0.0 }; 
-    let c = Complex::new(p.0, p.1);
-    let max_iter = 1000;
+//     // println!("{}   , {}",p.0,p.1);
+//     let mut z = Complex { re: 0.0, im: 0.0 }; 
+//     let c = Complex::new(p.0, p.1);
+//     let max_iter = 1000;
 
-    for i in 0..max_iter {
-        if (z.norm() > 2.0){
-            // println!("norm : {}  iters :  {}",z.norm(),i);
-            return i;
+//     for i in 0..max_iter {
+//         if (z.norm() > 2.0){
+//             // println!("norm : {}  iters :  {}",z.norm(),i);
+//             return i;
             
-        }
-        z = z*z +c;
-    }
-    return max_iter
-}
+//         }
+//         z = z*z +c;
+//     }
+//     return max_iter
+// }
 
 
 
 
-fn mandelbrotset(x_min:f64,x_max:f64,y_min:f64,y_max:f64,width:usize,height:usize){
+// fn mandelbrotset(x_min:f64,x_max:f64,y_min:f64,y_max:f64,width:usize,height:usize){
 
-    for h in 0..=height{
-        let mut line = String::with_capacity(width);
-        for w in 0..=width{
-            let x_percent = (w as f64 / width as f64);
-            let y_percent = (h as f64 / height as f64);
-            // println!("{} pecents {}",x_percent,y_percent);
-            let p = (x_min + (x_max - x_min) * x_percent, y_min + (y_max - y_min) * y_percent );
-            let val = match check_in_set(p){
-                0..=2 => ' ',
-                3..=5 => '.',
-                6..=10 => '•',
-                11..=30 => '*',
-                31..=100 => '+',
-                101..=200 => 'x',
-                201..=400 => '$',
-                401..=700 => '#',
-                _ => '%',
-              };
-        }
-        println!("{}", line);
-    } 
+//     for h in 0..=height{
+//         let mut line = String::with_capacity(width);
+//         for w in 0..=width{
+//             let x_percent = (w as f64 / width as f64);
+//             let y_percent = (h as f64 / height as f64);
+//             // println!("{} pecents {}",x_percent,y_percent);
+//             let p = (x_min + (x_max - x_min) * x_percent, y_min + (y_max - y_min) * y_percent );
+//             let val = match check_in_set(p){
+//                 0..=2 => ' ',
+//                 3..=5 => '.',
+//                 6..=10 => '•',
+//                 11..=30 => '*',
+//                 31..=100 => '+',
+//                 101..=200 => 'x',
+//                 201..=400 => '$',
+//                 401..=700 => '#',
+//                 _ => '%',
+//               };
+//         }
+//         println!("{}", line);
+//     } 
 
-}
-
-
+// }
 
 
 
-fn main() {
-    mandelbrotset(-2.0, 1.0, -1.0,1.0, 100, 24);
-}
-// use num::complex::Complex;    // <1>
+
+
+// fn main() {
+//     mandelbrotset(-2.0, 1.0, -1.0,1.0, 100, 24);
+// }
+// // use num::complex::Complex;    // <1>
 
 // fn calculate_mandelbrot(      // <2>
 
@@ -126,10 +126,19 @@ fn main() {
 //   }
 // }
 
-// fn main() {
+fn array_ex (){
+    let arr:[u8;4] = [0,0,0,2];
+    for it in arr {
+        println!("{}",it);
+    }
+
+}
+
+fn main() {
   
 //   let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.0,
 //                                         1.0, 100, 24);
 
 //   render_mandelbrot(mandelbrot);
-// }
+array_ex()
+}
---
+++ rust_in_action/cpu_proj/src/main.rs
@@ -0,0 +1,78 @@
+/*
+in computer bits 
+
+nibble - 4 bits  range in bin - 0-15  in hex - 0-0xF
+bytes - 8 bits  bin - 0-255 0 -oxFF
+each number in hex represents 4 bits
+so 0X f000 -  
+
+bitmasking
+
+We can turn a bit on by a bitwise OR ( | ) with 1 in the relevant position.
+We can test whether a bit is set by a bitwise AND (&) with 1 in the relevant position.
+We can turn a bit off by a bitwise AND (&) with NOT (∼) 1 in the relevant position.
+We can toggle a bit by a bitwise XOR (∧
+) with 1 in the relevant position.
+
+
+*/
+
+
+
+extern crate hex;
+struct CPU {
+
+    position_in_mem :usize;
+    regisers:[u8;16]; // 16 registers so we can use a single hex code to access a regiterr
+    memory:[u8;0x1000] //4096 bytes of ram
+
+}
+
+
+impl CPU {
+
+    fn read_opcode(&self){
+        let p = self.position_in_mem;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
+
+    }
+
+
+    fn parse_opcode(opcode:u16)->(u8,u8,u8,u8){
+       
+        let c = ((opcode_bin & 0xF000)>>12);
+        let x = (opcode & 0x0F00) >> 8;
+        let y = (opcode & 0x00F0) >> 4; 
+        let d = (opcode & 0x000F) >> 0;
+
+        return (c,x,y,d)
+
+    }
+    fn add_xy(&mut self,x:u8,y:u8){
+
+    }
+
+    fn run (&self){
+        loop {
+
+             let (c,x,y,d) = parse_opcode(self.curren_op);
+
+             match(c,x,y,d){
+                ()
+
+             }
+
+
+        }
+
+    }
+
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
