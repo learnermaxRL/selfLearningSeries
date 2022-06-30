9ce8e952017466622fd8075753b91aefeffabc71
learnermaxrl <sharma.mayank2125@gmail.com>
2022-07-01 03:25:15 +0530

    asving changes

---
+++ os_rust/os/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+static HELLO: &u8 = b"Hello World!";
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vg_bufer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
